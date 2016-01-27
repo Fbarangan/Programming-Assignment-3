40de1b71cf465f7fcbb692f0b21058c7ebb1dae7
Fbarangan <franb86.2@gmail.com>
2016-01-27 14:12:46 -0500

    Initial commit

--- Assingment_3_1.R
+++ Assingment_3_1.R
@@ -5,7 +5,7 @@
 
 30_day_Mortality_rates <- function("outcome-of-care-measures.csv"){
 
-        outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        outcome <- read.csv("outcome-of-care-measures.csv", colclasses = "character")
 
         ## Coerce to numeric
         outcome_11 <- as.numeric(outcome[,11])
@@ -13,3 +13,11 @@
 
 hist(outcome_11)
 }
+###############################
+## This function will determine if you have a valid State code
+validState <- function(StateID = State){
+        desiredState <- state.abb[match(StateID, state.abb)]
+        print(desiredState)
+}
+###############################
+
