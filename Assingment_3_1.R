8de7d88b88c50692123fd83bab4fcc8424453ad8
Fbarangan <franb86.2@gmail.com>
2016-01-30 03:44:00 -0500

    almost. done been using mostly dplyr

--- Assingment_3_1.R
+++ Assingment_3_1.R
@@ -5,12 +5,11 @@
 
 30_day_Mortality_rates <- function(){
 
-        outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+        outcome <- read.csv("outcome-of-care-measures.csv")
+        outcome <- as.integer (outcome[,11])
 
         ## Coerce to numeric
-        outcome_11 <- as.numeric(outcome[,11])
-
-
+        outcome <- as.numeric(outcome[,11])
        hist(outcome_11)
 }
 ###############################
@@ -42,13 +41,33 @@ orderOutcome <- outcome[order(outcome[,column]),]
 ######################################
 ### Using dplyr function
 ### selecting  desired column from the main data - outcome
-selectOutcome <-  function(desired_File, desired_Columns = c(1:10)){
-         selectedColumn  <-  head(select (desired_File, desired_Columns))
+selectOutcome <-  function(desired_File, desired_Columns = c(2,7,11, 19)){
+         selectedColumn  <-  select (desired_File, desired_Columns)
          selectedColumn <<- selectedColumn
         print(selectedColumn)
         class(selectedColumn)
 }
-##########################################
+## Change data.frame into tble tbl_df
+selectedColumn <- tbl_df (selectedColumn)
+
+# Change a column into integer
+as.integer (selectedColumn$Heart_Attack)
+
+
+#########################################     2, 7,11 , 19
+# Renaming columns
+colnames(selectedColumn)[1] = "Hospital_Name"
+colnames(selectedColumn)[3] = "Heart_Attack"
+colnames(selectedColumn)[4] = "Heart_Failure"
+
+# Filter by Hospital Name
+filter(selectedColumn, Hospital_Name == "ST LUKE'S HOSPITAL AT THE VINTAGE, LLC")
+
+# Selecting state
+state_Select <- filter(selectedColumn, State == "TX" )
+
+# Arrange file after State selection
+
 
 # This is how you will sord when you find the selected columns
 orderdata <- selectedColumn[order(decreasing = FALSE, selectedColumn[,2]),]
